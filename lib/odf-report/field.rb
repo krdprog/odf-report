1c3ad90d7ac450aa4a9fb9494ce9bcb982c8246d
Alexey Tsaplin-Kupaysinov <info@krdprog.ru>
2023-11-18 09:28:14 +0300

    Fix delimeters

--- lib/odf-report/field.rb
+++ lib/odf-report/field.rb
@@ -1,7 +1,7 @@
 module ODFReport
   class Field
 
-    DELIMITERS = %w([ ])
+    DELIMITERS = %w(&lt; &gt;)
 
     def initialize(opts, &block)
       @name = opts[:name]
@@ -27,9 +27,9 @@ module ODFReport
 
     def to_placeholder
       if DELIMITERS.is_a?(Array)
-        "#{DELIMITERS[0]}#{@name.to_s.upcase}#{DELIMITERS[1]}"
+        "#{DELIMITERS[0]}#{@name.to_s}#{DELIMITERS[1]}"
       else
-        "#{DELIMITERS}#{@name.to_s.upcase}#{DELIMITERS}"
+        "#{DELIMITERS}#{@name.to_s}#{DELIMITERS}"
       end
     end
 
--- lib/odf-report/version.rb
+++ lib/odf-report/version.rb
@@ -1,3 +1,3 @@
 module ODFReport
-  VERSION = "0.7.3"
+  VERSION = "0.7.3.1"
 end
